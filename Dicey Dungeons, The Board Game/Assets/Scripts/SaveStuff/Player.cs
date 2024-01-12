fb9c72e9ad08299b4f43430c35b46d326cd4275b
JosephBlom <24.joseph.blom@ksd.org>
2024-01-12 13:49:40 -0800

    Update Player.cs

--- Dicey Dungeons, The Board Game/Assets/Scripts/SaveStuff/Player.cs
+++ Dicey Dungeons, The Board Game/Assets/Scripts/SaveStuff/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     public Vector3 position;
     public string[] activeCardNames;
     public List<CardSO> activeCards = new List<CardSO>();
+    public int health;
     private void Start()
     {
         position = transform.position;
